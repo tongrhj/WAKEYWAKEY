ba11586432998e8bc58c8e363f6c0c5b4cd7c385
tongrhj <tongrhj@gmail.com>
2016-01-24 12:11:45 +0800

    Removed test dependencies and Created Readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
+# WAKEY WAKEY
+### If you don't get out of bed in time, WAKEY WAKEY posts your sleeping face to the world.
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-green.svg?style=flat-square)](https://github.com/feross/standard)
 
+## Installation
+```
+npm install
+```
+
+## To Run Watch
+```
+npm run dev
+```
+## What WAKEY WAKEY Does
+* Facebook Login
+* Starts alarm countdown, can be cancelled
+* Use WebRTC to capture a photo of you sleeping after 5 second (demo-only)/ 8 hours
+* Converts image to Base64 and posts it to MongoLabs
+* Displays all incriminating images from MongoLabs
+
+## Tools and Frameworks
+* Express
+* WebRTC
+* Fetch API
+* Auth0
+* MongoLabs
+
+## Product Roadmap
+* UX Design
+* Countdown Timer with Alarm Clock Sound
+* Switch image db to Cloudinary
--- public/js/index.js
+++ public/js/index.js
@@ -102,6 +102,7 @@ function takepicture () {
   }
 }
 
+
 function retrievePictures () {
   // Fetch the pictures so user is shamed since they didnt wake in time
   fetch('/gallery', {
@@ -123,99 +124,3 @@ function retrievePictures () {
 }
 
 window.addEventListener('load', startup)
-
-// 'use strict'
-//
-// const width = 320    // We will scale the photo width to this
-// let height = 0       // This will be computed based on the input stream
-//
-// let streaming = false
-//
-// let video = null
-// let canvas = null
-// let photo = null
-// let startbutton = null
-//
-// function startup () {
-//   video = document.getElementById('capture-video')
-//   canvas = document.getElementById('capture-canvas')
-//   photo = document.getElementById('capture-photo')
-//   startbutton = document.getElementById('capture-startbtn')
-//
-//   navigator.getMedia = (navigator.getUserMedia ||
-//                         navigator.webkitGetUserMedia ||
-//                         navigator.mozGetUserMedia ||
-//                         navigator.msGetUserMedia)
-//
-//   navigator.getMedia(
-//     {video: true, audio: false},
-//     (stream) => {
-//       if (navigator.mozGetUserMedia) {
-//         video.mozSrcObject = stream
-//       } else {
-//         var vendorURL = window.URL || window.webkitURL
-//         video.src = vendorURL.createObjectURL(stream)
-//       }
-//       video.play()
-//     },
-//     (err) => console.log('An error occured! ' + err)
-//   )
-//
-//   video.addEventListener('canplay', () => {
-//     if (!streaming) {
-//       height = video.videoHeight / (video.videoWidth / width)
-//
-//       if (isNaN(height)) height = width / (4 / 3)
-//
-//       video.setAttribute('width', width)
-//       video.setAttribute('height', height)
-//       canvas.setAttribute('width', width)
-//       canvas.setAttribute('height', height)
-//       streaming = true
-//     }
-//   }, false)
-//
-//   startbutton.addEventListener('click', (e) => {
-//     takepicture()
-//     e.preventDefault()
-//   }, false)
-//
-//   clearphoto()
-// }
-//
-// function clearphoto () {
-//   var context = canvas.getContext('2d')
-//   context.fillStyle = '#AAA'
-//   context.fillRect(0, 0, canvas.width, canvas.height)
-//
-//   var data = canvas.toDataURL('image/png')
-//   photo.setAttribute('src', data)
-// }
-//
-// function takepicture () {
-//   var context = canvas.getContext('2d')
-//   if (width && height) {
-//     canvas.width = width
-//     canvas.height = height
-//     context.drawImage(video, 0, 0, width, height)
-//     var data = canvas.toDataURL('image/png')
-//     photo.setAttribute('src', data)
-//
-//     fetch('/gallery', {
-//       headers: {
-//         'Authorization': 'Bearer ' + localStorage.getItem('userToken')
-//       },
-//       method: 'POST',
-//       body: {name: userProfile.given_name, URL: data},
-//       cache: false
-//     })
-//     .catch((error) => {
-//       console.log('There has been a problem with your fetch operation: ' + error.message)
-//     })
-//
-//   } else {
-//     clearphoto()
-//   }
-// }
-//
-// window.addEventListener('load', startup)
