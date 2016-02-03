19fc9814613020dc2b4d41e0f724eeb027a3fdc7
tongrhj <tongrhj@gmail.com>
2016-02-03 17:49:45 +0800

    Updated to 1200px

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 'use strict'
 /* global Audio fetch Request localStorage FB userProfile */
 
-const width = 320    // We will scale the photo width to this
+const width = 1200    // We will scale the photo width to this
 let height = 0       // This will be computed based on the input stream
 
 let streaming = false
@@ -94,104 +94,91 @@ function cancelCountdown () {
   cancelbutton.classList.add('hidden')
 }
 
+function uploadToMongo (dataURL) {
+  const mongoRequest = new Request('/gallery', {
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('userToken'),
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      name: userProfile.given_name,
+      url: dataURL
+    }),
+    cache: false
+  })
+  fetch(mongoRequest)
+  .then(() => {
+    retrievePictures()
+  })
+  .catch((error) => {
+    console.log('There has been a problem with your fetch operation: ' + error.message)
+  })
+}
+
+function uploadToIMGUR (base64image) {
+  const imgurRequest = new Request('https://api.imgur.com/3/image', {
+    headers: {
+      'Authorization': 'Client-ID 4b24a6a46e6bea7',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      image: base64image,
+      type: 'base64',
+      description: 'Generated from WAKEYWAKEY Alarm Clock Webapp'
+    }),
+    cache: true
+  })
+  fetch(imgurRequest)
+  .then(checkStatus)
+  .then(parseJSON)
+  .then((res) => {
+    var imageURL = res.data.link
+    FB.ui({
+      method: 'feed',
+      name: 'I Did Not Wake Up On Time',
+      link: 'https://wakey2.herokuapp.com/',
+      caption: 'WakeyWakey Alarm Clock Webapp',
+      picture: imageURL,
+      description: 'I failed to wake up this morning and this is my punishment. Shame! Shame! Shame!'
+    }, (res) => { console.log(res) })
+  }).catch((error) => {
+    console.log('Upload to IMGUR Failed: ' + error.message)
+  })
+}
+
+function checkStatus (response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON (response) {
+  return response.json()
+}
+
 function takepicture () {
   const context = canvas.getContext('2d')
   if (width && height) {
     canvas.width = width
     canvas.height = height
     context.drawImage(video, 0, 0, width, height)
-    var dataURL = canvas.toDataURL('image/png')
+    const dataURL = canvas.toDataURL('image/png')
     photo.setAttribute('src', dataURL)
-
     const base64image = dataURL.toString().substring(22, dataURL.toString().length)
-
-    const mongoRequest = new Request('/gallery', {
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('userToken'),
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        name: userProfile.given_name,
-        url: dataURL
-      }),
-      cache: false
-    })
-
-    const imgurRequest = new Request('https://api.imgur.com/3/image', {
-      headers: {
-        'Authorization': 'Client-ID 4b24a6a46e6bea7',
-        'Content-Type': 'application/json'
-      },
-      method: 'post',
-      body: JSON.stringify({
-        image: base64image,
-        type: 'base64',
-        description: 'Generated from WAKEYWAKEY Alarm Clock Webapp'
-      }),
-      cache: true
-    })
-
-    function checkStatus (response) {
-      if (response.status >= 200 && response.status < 300) {
-        return response
-      } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
-      }
-    }
-
-    function parseJSON (response) {
-      return response.json()
-    }
-
-    // Try Posting to IMGUR
-    // process.env.WAKEY_IMGUR_CLIENT_ID
-    // process.env.WAKEY_IMGUR_CLIENT_SECRET
-    fetch(imgurRequest)
-    .then(checkStatus)
-    .then(parseJSON)
-    .then((res) => {
-      var imageURL = res.data.link
-
-      FB.ui({
-        method: 'feed',
-        name: 'I Did Not Wake Up On Time',
-        link: 'https://wakey2.herokuapp.com/',
-        caption: 'WakeyWakey Alarm Clock Webapp',
-        picture: imageURL,
-        description: 'I failed to wake up this morning and this is my punishment. Shame! Shame! Shame!'
-      }, (res) => { console.log(res) })
-
-    }).catch((error) => {
-      console.log('Upload to IMGUR Failed: ' + error.message)
-    })
-
-    // Try Posting to Facebook
-    // FB.ui({
-    //   method: 'feed',
-    //   name: 'I Did Not Wake Up On Time',
-    //   link: 'https://wakey2.herokuapp.com/',
-    //   caption: 'WakeyWakey Alarm Clock Webapp',
-    //   picture: 'https://wakey2.herokuapp.com/img/wakeywakey.png',
-    //   description: 'I failed to wake up this morning and this is my punishment. Shame! Shame! Shame!'
-    // }, (res) => { console.log(res) })
-
-    fetch(mongoRequest)
-    .then(() => {
-      retrievePictures()
-    })
-    .catch((error) => {
-      console.log('There has been a problem with your fetch operation: ' + error.message)
-    })
+    uploadToMongo(dataURL)
+    uploadToIMGUR(base64image)
   } else {
     clearphoto()
   }
 }
 
 function retrievePictures () {
-  // Fetch the pictures so user is shamed since they didnt wake in time
   fetch('/gallery', {
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('userToken')
